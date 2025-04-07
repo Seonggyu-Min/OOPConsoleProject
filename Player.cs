e669faa715fbfa8bff699cca39e58c00839f99ef
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 01:19:47 +0900

    [feat] 플레이어 클래스 생성
    플레이어 클래스 생성 및 Move 메서드 추가

--- Player.cs
+++ Player.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace OOPConsoleProject
 {
-    class Player
+    public class Player
     {
+        public int PosX { get; private set; }
+        public int PosY { get; private set; }
+
+        public Player(int x, int y)
+        {
+            PosX = x;
+            PosY = y;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            PosX += dx;
+            PosY += dy;
+        }
     }
 }
--- VectorStruct.cs
+++ VectorStruct.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace OOPConsoleProject
 {
-    class VectorStruct
+    public struct Vector2
     {
+        public int x;
+        public int y;
+
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static bool operator ==(Vector2 left, Vector2 right)
+        {
+            return left.x == right.x && left.y == right.y;
+        }
+
+        public static bool operator !=(Vector2 left, Vector2 right)
+        {
+            return left.x != right.x || left.y != right.y;
+        }
     }
 }
