4298b9c6276d4020140420ad9d0337878b682226
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 01:01:48 +0900

    [feat] Ship 맵 배열 구현, vector 비교 연산자 오버로딩
    Ship 클래스 생성 및 맵 배열 구현
    플레이어와 다른 객체의 위치가 같은지 확인하는 연산자 오버로딩 구현

---
+++ Ship.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+
+    public class Ship
+    {
+        private char[,] mapData = new char[,]
+        {
+                { '#', '#', '#', '#', '#', '#', '#' },
+                { '#', ' ', '#', ' ', '#', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', ' ', '#', ' ', '#', ' ', '#' },
+                { '#', '#', '#', '#', '#', '#', '#' },
+        };
+
+        private bool[,] map = new bool[7, 7];
+
+        
+
+        public Ship()
+        {
+            for (int y = 0; y < mapData.GetLength(0); y++)
+            {
+                for (int x = 0; x < mapData.GetLength(1); x++)
+                {
+                    map[y, x] = mapData[y, x] == '#' ? false : true;
+                }
+            }
+
+
+
+        }
+    }
+
+}
+
