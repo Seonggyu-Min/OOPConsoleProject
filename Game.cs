d1f0e8f170b87214ad986030efd983f2461f18b7
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 00:35:15 +0900

    [feat] 게임루프 추가
    Game 클래스 및 그에 해당하는 기초적 게임루프 추가

---
+++ Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    class Game
+    {
+        private static bool gameOver;
+
+        public static void Start()
+        {
+            Console.CursorVisible = false;
+
+            gameOver = false;
+        }
+
+        public static void Run()
+        {
+            Start();
+
+            while (!gameOver)
+            {
+
+            }
+        }
+
+        public static void End()
+        {
+            gameOver = true;
+        }
+    }
+}
