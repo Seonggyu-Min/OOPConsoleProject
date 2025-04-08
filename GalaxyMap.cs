ef18385aa94aa81407f3b897c7a03a34086fe14f
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 18:27:16 +0900

    [feat] GalaxyMap의 노드에 정보 추가
    GalaxyMap의 노드에 정보 추가

--- GalaxyMap.cs
+++ GalaxyMap.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace OOPConsoleProject
 {
-    
+
     public class GalaxyMap
     {
         public char[,] MapData { get; private set; }
         public bool[,] Map { get; private set; }
 
-        public Dictionary<(int x, int y), GalaxyLocation> GalaxyRoom { get; private set; }
+        public Dictionary<(int x, int y), GalaxyNodeInfo> GalaxyRoom { get; private set; }
 
         public GalaxyMap()
         {
@@ -45,22 +45,22 @@ namespace OOPConsoleProject
                 }
             }
 
-            GalaxyRoom = new Dictionary<(int x, int y), GalaxyLocation>
+            GalaxyRoom = new Dictionary<(int x, int y), GalaxyNodeInfo>
             {
-                { (5, 1), GalaxyLocation.Start }, // 0번 Start
-                
-                { (3, 3), GalaxyLocation.Fuel }, // 1번
-                { (7, 3), GalaxyLocation.Encounter },
-                { (3, 5), GalaxyLocation.Oxygen },
-                { (5, 5), GalaxyLocation.Encounter },
-                { (7, 5), GalaxyLocation.Oxygen },
-                { (3, 7), GalaxyLocation.Fuel },
-                { (5, 7), GalaxyLocation.Fuel },
-                { (7, 7), GalaxyLocation.Encounter },
-                { (4, 9), GalaxyLocation.Encounter },
-                { (7, 9), GalaxyLocation.Encounter }, // 10번
+                { (5, 1), new GalaxyNodeInfo(0, "Start", "시작 지점", "무사히 살아남아 지구로 귀환하세요.") }, // 0번 Start
 
-                { (6, 11), GalaxyLocation.End }, // 11번 End
+                { (3, 3), new GalaxyNodeInfo(1, "Fuel", "연료 충전 지점", "이 은하는 연료가 풍부해보입니다. 근처에 들러 연료를 충전할 수 있을 것 같습니다.") },
+                { (7, 3), new GalaxyNodeInfo(2, "Encounter", "외계 문명 조우 지점", "이 은하에는 발전된 문명이 살고 있습니다. 근처에 들러 대화를 나눠볼 수 있을 것 같습니다.") },
+                { (3, 5), new GalaxyNodeInfo(3, "Oxygen", "산소 충전 지점", "이 은하에는 산소가 풍부해보입니다. 근처에 들러 산소를 충전할 수 있을 것 같습니다.") },
+                { (5, 5), new GalaxyNodeInfo(4, "Encounter", "외계 문명 조우 지점", "이 은하에는 발전된 문명이 살고 있습니다. 근처에 들러 대화를 나눠볼 수 있을 것 같습니다.") },
+                { (7, 5), new GalaxyNodeInfo(5, "Oxygen", "산소 충전 지점", "이 은하에는 산소가 풍부해보입니다. 근처에 들러 산소를 충전할 수 있을 것 같습니다.") },
+                { (3, 7), new GalaxyNodeInfo(6, "Fuel", "연료 충전 지점", "이 은하는 연료가 풍부해보입니다. 근처에 들러 연료를 충전할 수 있을 것 같습니다.") },
+                { (5, 7), new GalaxyNodeInfo(7, "Fuel", "연료 충전 지점", "이 은하는 연료가 풍부해보입니다. 근처에 들러 연료를 충전할 수 있을 것 같습니다.") },
+                { (7, 7), new GalaxyNodeInfo(8, "Encounter", "외계 문명 조우 지점", "이 은하에는 발전된 문명이 살고 있습니다. 근처에 들러 대화를 나눠볼 수 있을 것 같습니다.") },
+                { (4, 9), new GalaxyNodeInfo(9, "Encounter", "외계 문명 조우 지점", "이 은하에는 발전된 문명이 살고 있습니다. 근처에 들러 대화를 나눠볼 수 있을 것 같습니다.") },
+                { (7, 9), new GalaxyNodeInfo(10, "Encounter", "외계 문명 조우 지점", "이 은하에는 발전된 문명이 살고 있습니다. 근처에 들러 대화를 나눠볼 수 있을 것 같습니다.") },
+
+                { (6, 11), new GalaxyNodeInfo(11, "End", "도착 지점", "지구가 눈 앞에 보이기 시작합니다.") },
             };
         }
 
