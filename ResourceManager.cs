cd521af6b0ebc15e1f2a0f9d49f81a6361b17bea
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 09:46:54 +0900

    [feat] ResourceManager 추가
    ResourceManager 클래스 추가 및 Consume 메서드 추가

---
+++ ResourceManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    class ResourceManager
+    {
+        public static int Fuel { get; set; }
+        public static int Oxygen { get; set; }
+
+        public void ConsumeFuel (int amount)
+        {
+            Fuel -= amount;
+            if (Fuel <= 0)
+            {
+
+            }
+        }
+
+
+        public void ConsumeOxygen(int amount)
+        {
+            Oxygen -= amount;
+            if (Oxygen <= 0)
+            {
+
+            }
+        }
+
+    }
+}
