85df64a9e2642aa1223555000d3906262d6a8233
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 11:35:28 +0900

    [feat] ShipScene 추상 메서드 구현
    ShipScene 추상 메서드 구현

---
+++ Scenes/ShipScene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject.Scenes
+{
+    public class ShipScene : BaseScene
+    {
+        private ConsoleKey input;
+        Ship ship;
+        Player player;
+
+        public ShipScene() 
+        {
+            name = "Ship";
+        }
+
+        public override void Render()
+        {
+            Console.Clear();
+            ship.PrintMap();
+        }
+        public override void Input()
+        {
+            input = Console.ReadKey(true).Key;
+        }
+        public override void Update()
+        {
+            player.Move(input);
+        }
+
+        public override void Result()
+        {
+
+        }
+    }
+}
