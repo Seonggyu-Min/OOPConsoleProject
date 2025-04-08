8bdc0d6138549b6fc2e8a42ff2e273c5c855da7f
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 18:28:54 +0900

    [feat] ResourceManager 기능 추가
    ResourceManager 정적 클래스화 및 메서드 수정

--- ResourceManager.cs
+++ ResourceManager.cs
@@ -6,28 +6,36 @@ using System.Threading.Tasks;
 
 namespace OOPConsoleProject
 {
-    class ResourceManager
+    public static class ResourceManager
     {
         public static int Fuel { get; set; }
         public static int Oxygen { get; set; }
 
-        public void ConsumeFuel (int amount)
-        {
-            Fuel -= amount;
-            if (Fuel <= 0)
-            {
+        public static int MaxFuel { get; set; }
+        private static int MaxOxygen { get; set; }
 
-            }
+        static ResourceManager()
+        {
+            Fuel = 6;
+            Oxygen = 6;
+
+            MaxFuel = 15;
+            MaxOxygen = 15;
+        }
+
+        public static void ChargeFuel (int amount)
+        {
+            Fuel += amount;
+            if (Fuel > MaxFuel)
+                Fuel = 15;
         }
 
 
-        public void ConsumeOxygen(int amount)
+        public static void ChargeOxygen(int amount)
         {
-            Oxygen -= amount;
-            if (Oxygen <= 0)
-            {
-
-            }
+            Oxygen += amount;
+            if (Oxygen > MaxOxygen)
+                Oxygen = 15;
         }
 
     }
