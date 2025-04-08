a830e0fad3a17ea544d8ec1b59da47cea82aac90
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 17:11:31 +0900

    [feat] GalaxyMap 클래스 추가 및 메서드 구현
    GalaxyMap 클래스 추가
    char[,] / bool[,] 배열 구현
    GalaxyRoom Dictionary 추가
    PrintGalaxyMap() 메서드 추가

---
+++ GalaxyMap.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    
+    public class GalaxyMap
+    {
+        public char[,] MapData { get; private set; }
+        public bool[,] Map { get; private set; }
+
+        public Dictionary<(int x, int y), GalaxyLocation> GalaxyRoom { get; private set; }
+
+        public GalaxyMap()
+        {
+            MapData = new char[,]
+            {
+                { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+                { '#', ' ', ' ', ' ', ' ', 'S', ' ', ' ', ' ', ' ', '#' },
+                { '#', ' ', ' ', ' ', '/', ' ', '\\', ' ', ' ', ' ', '#' },
+                { '#', ' ', ' ', '@', ' ', ' ', ' ', '@', ' ', ' ', '#' },
+                { '#', ' ', ' ', '/', ' ', '\\', ' ', '|', ' ', ' ', '#' },
+                { '#', ' ', ' ', '@', ' ', '@', ' ', '@', ' ', ' ', '#' },
+                { '#', ' ', ' ', '|', ' ', '|', '\\', '|', ' ', ' ', '#' },
+                { '#', ' ', ' ', '@', ' ', '@', ' ', '@', ' ', ' ', '#' },
+                { '#', ' ', ' ', ' ', '\\', '/', ' ', '|', ' ', ' ', '#' },
+                { '#', ' ', ' ', ' ', '@', ' ', ' ', '@', ' ', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', '\\', '/', ' ', ' ', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', 'E', ' ', ' ', ' ', '#' },
+                { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
+            };
+
+
+            Map = new bool[13, 11];
+
+            for (int y = 0; y < MapData.GetLength(0); y++)
+            {
+                for (int x = 0; x < MapData.GetLength(1); x++)
+                {
+                    Map[y, x] = MapData[y, x] == '#' ? false : true;
+                }
+            }
+
+            GalaxyRoom = new Dictionary<(int x, int y), GalaxyLocation>
+            {
+                { (5, 1), GalaxyLocation.Start }, // 0번 Start
+                
+                { (3, 3), GalaxyLocation.Fuel }, // 1번
+                { (7, 3), GalaxyLocation.Encounter },
+                { (3, 5), GalaxyLocation.Oxygen },
+                { (5, 5), GalaxyLocation.Encounter },
+                { (7, 5), GalaxyLocation.Oxygen },
+                { (3, 7), GalaxyLocation.Fuel },
+                { (5, 7), GalaxyLocation.Fuel },
+                { (7, 7), GalaxyLocation.Encounter },
+                { (4, 9), GalaxyLocation.Encounter },
+                { (7, 9), GalaxyLocation.Encounter }, // 10번
+
+                { (6, 11), GalaxyLocation.End }, // 11번 End
+            };
+        }
+
+        public void PrintGalaxyMap()
+        {
+            for (int y = 0; y < MapData.GetLength(0); y++)
+            {
+                for (int x = 0; x < MapData.GetLength(1); x++)
+                {
+                    Console.Write(MapData[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
