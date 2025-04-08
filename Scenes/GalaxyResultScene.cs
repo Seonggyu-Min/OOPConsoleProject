9abefbb7883e0acf5d8efed0d85b7675928693cc
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 18:30:46 +0900

    [feat] GalaxyResultScene 클래스 추가
    GalaxyResultScene 클래스 추가 및 오버라이딩 뼈대 구현

---
+++ Scenes/GalaxyResultScene.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject.Scenes
+{
+    public class GalaxyResultScene : BaseScene
+    {
+        public GalaxyResultScene(GalaxyNodeInfo nodeInfo)
+        {
+
+        }
+
+        public override void Render()
+        {
+
+        }
+        public override void Input()
+        {
+
+        }
+        public override void Update()
+        {
+
+
+        }
+        public override void Result()
+        {
+
+        }
+
+
+    }
+}
