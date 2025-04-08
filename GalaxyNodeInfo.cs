d9b8587029d6d842d6af7a4ed99a05573f79ed00
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-08 18:26:38 +0900

    [feat] GalaxyLocation enum 삭제 및 GalaxyNodeInfo 클래스로 역할 이동
    GalaxyLocation enum을 삭제하여GalaxyNodeInfo 클래스에 추가 정보 기입

---
+++ GalaxyNodeInfo.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public class GalaxyNodeInfo
+    {
+        public int Id { get; set; }
+        public string LocationType { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public GalaxyNodeInfo(int id, string type, string name, string description)
+        {
+            Id = id;
+            LocationType = type;
+            Name = name;
+            Description = description;
+        }
+    }
+}
