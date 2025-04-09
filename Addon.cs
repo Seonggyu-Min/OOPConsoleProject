cee23c83132fbee26b87d843b4db0841ffa03f00
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-09 14:31:14 +0900

    [feat] Addon 클래스 추가 및 필드 생성
    Addon 클래스 추가 및 필드 생성

---
+++ Addon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public class Addon
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Grade { get; set; }
+        public string Description { get; set; }
+
+        public Addon(string name, string type, int grade, string description)
+        {
+            Name = name;
+            Type = type;
+            Grade = grade;
+            Description = description;
+        }
+    }
+}
