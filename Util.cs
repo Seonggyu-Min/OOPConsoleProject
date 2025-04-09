6d3e6966c2c925523d0913448067c6f2bd453198
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-10 02:06:13 +0900

    [feat] 유틸용 메서드 추가
    오버로딩된 PrintText, DelayedText, ReadKey 메서드 추가

--- Util.cs
+++ Util.cs
@@ -12,19 +12,59 @@ namespace OOPConsoleProject
         {
             if (input == ConsoleKey.Z || input == ConsoleKey.Escape)
             {
-                Console.WriteLine("뒤로 갑니다 - 디버그용 문구");
-                Thread.Sleep(500);
+                Console.WriteLine("뒤로 갑니다");
+                ReadKey();
                 Game.PopScene();
             }
         }
         public static void EscapeSceneWOKey()
         {
-            Console.WriteLine("뒤로 갑니다 - 디버그용 문구");
-            Thread.Sleep(500);
+            Console.WriteLine("뒤로 갑니다");
+            ReadKey();
             Game.PopScene();
         }
 
 
-        public static void PrintText(string text) { }
+        public static void PrintText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void PrintText(string text, int time)
+        {
+            Console.WriteLine(text);
+            Thread.Sleep(time);
+        }
+
+        public static void PrintText(string text, ConsoleColor textColor)
+        {
+            Console.ForegroundColor = textColor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void PrintText(string text, int time, ConsoleColor textColor)
+        {
+            Console.ForegroundColor = textColor;
+            Thread.Sleep(time);
+            Console.WriteLine(text); 
+            Console.ResetColor();
+        }
+
+        public static void DelayedText(string text)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(50);
+            }
+        }
+
+        public static void ReadKey()
+        {
+            Console.WriteLine();
+            Util.PrintText("아무 키나 누르세요...", ConsoleColor.Green);
+            Console.ReadKey(true);
+        }
     }
 }
