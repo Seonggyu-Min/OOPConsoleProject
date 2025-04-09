4b5d9ee20d567dd4dc5622536cbffc1c584a5eda
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-09 16:41:48 +0900

    [feat] 플레이어블 드론 기능 추가
    CollectMapDrone 클래스 및 FuelCollectScene, OxygenCollectScene에서 플레이 할 수 있는 드론 기능 추가

---
+++ CollectMapDrone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public class CollectMapDrone
+    {
+        public Vector2 DronePosition { get; set; }
+        public bool[,] IsMovableMap { get; set; }
+
+        public CollectMapDrone(int x, int y) 
+        {
+            DronePosition = new Vector2(x, y);
+        }
+
+        public void Print()
+        {
+            Console.SetCursorPosition(DronePosition.x, DronePosition.y);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write('D');
+            Console.ResetColor();
+        }
+
+        public void Move(ConsoleKey input)
+        {
+            Vector2 targetPos = DronePosition;
+            switch (input)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    targetPos.y--;
+                    break;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    targetPos.y++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    targetPos.x--;
+                    break;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    targetPos.x++;
+                    break;
+            }
+
+            if (IsMovableMap[targetPos.y, targetPos.x] == true)
+            {
+                DronePosition = targetPos;
+            }
+        }
+
+    }
+}
