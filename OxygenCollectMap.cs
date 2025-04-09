3227799f122348acdc0ad9e9d1180ee6f8a6c4ce
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-09 16:40:38 +0900

    [feat] 산소 포집 맵 추가
    FuelCollectMap 클래스 추가 및 산소 포집 맵 추가

---
+++ OxygenCollectMap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConsoleProject
+{
+    public class OxygenCollectMap
+    {
+        public char[,] MapData { get; private set; }
+        public bool[,] Map { get; private set; }
+
+        public OxygenCollectMap()
+        {
+            MapData = new char[7, 7]
+            {
+                { '#', '#', '#', '#', '#', '#', '#' },
+                { '#', ' ', '#', '&', '#', '&', '#' },
+                { '#', ' ', ' ', ' ', '&', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', ' ', '&', ' ', ' ', ' ', '#' },
+                { '#', ' ', '#', ' ', '#', '&', '#' },
+                { '#', '#', '#', '#', '#', '#', '#' },
+            };
+
+            Map = new bool[7, 7];
+
+            for (int y = 0; y < MapData.GetLength(0); y++)
+            {
+                for (int x = 0; x < MapData.GetLength(1); x++)
+                {
+                    Map[y, x] = MapData[y, x] == '#' ? false : true;
+                }
+            }
+        }
+
+        public void PrintOxygenMap()
+        {
+            for (int y = 0; y < MapData.GetLength(0); y++)
+            {
+                for (int x = 0; x < MapData.GetLength(1); x++)
+                {
+                    Console.Write(MapData[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
