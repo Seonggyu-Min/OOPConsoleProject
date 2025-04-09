ad789374e4ebad7a17bac30d2964c249dbbceb81
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-09 14:32:08 +0900

    [feat] 연료, 산소 소모 기능 구현
    연료, 산소 소모 기능 구현

--- Scenes/GalaxyResultScene.cs
+++ Scenes/GalaxyResultScene.cs
@@ -8,14 +8,20 @@ namespace OOPConsoleProject.Scenes
 {
     public class GalaxyResultScene : BaseScene
     {
+        GalaxyNodeInfo nodeInfo;
         public GalaxyResultScene(GalaxyNodeInfo nodeInfo)
         {
-
+            name = "GalaxyResult";
+            this.nodeInfo = nodeInfo;
         }
 
         public override void Render()
         {
-
+            Console.WriteLine($"{nodeInfo.Name} 은하계로 이동합니다.");
+            Console.WriteLine("연료가 3만큼 소모되었습니다.");
+            Console.WriteLine("산소가 3만큼 소모되었습니다.");
+            Console.WriteLine();
+            Console.WriteLine($"현재 남은 연료: {ResourceManager.Fuel - 3} / 현재 남은 산소: {ResourceManager.Oxygen - 3}");
         }
         public override void Input()
         {
@@ -23,14 +29,14 @@ namespace OOPConsoleProject.Scenes
         }
         public override void Update()
         {
-
-
+            ResourceManager.ConsumeFuel(3);
+            ResourceManager.ConsumeOxygen(3);
+            TravelState.Warp(nodeInfo.Id);
         }
         public override void Result()
         {
-
+            Game.ChangeScene("Ship");
+            Game.SceneStackClear();
         }
-
-
     }
 }
