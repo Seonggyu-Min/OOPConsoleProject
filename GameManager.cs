6c40ebfbb23736fb6f79ee9e5a67880dea039130
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-10 12:49:54 +0900

    [chore] 콘솔 메시지 수정
    콘솔 메시지 수정

--- GameManager.cs
+++ GameManager.cs
@@ -13,7 +13,7 @@ namespace OOPConsoleProject
             Util.PrintText(reason, 100, ConsoleColor.Red);
             Util.DelayedText("GAME OVER");
             Console.WriteLine();
-            Util.PrintText("아무 키나 눌러 다음으로...", 100, ConsoleColor.Red);
+            Util.PrintText("아무 키나 눌러 끝냅니다...", 100, ConsoleColor.Red);
             Console.ReadKey(true);
             Game.gameOver = true;
         }
